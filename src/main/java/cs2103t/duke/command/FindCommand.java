4a41568f207c44fb3de365c740219c378e8e87a4
kxyoke <kxyoke@gmail.com>
2019-09-11 18:01:29 +0800

    Do slight refactoring

--- src/main/java/cs2103t/duke/command/FindCommand.java
+++ src/main/java/cs2103t/duke/command/FindCommand.java
@@ -7,23 +7,37 @@ import cs2103t.duke.ui.Ui;
 
 import java.util.List;
 
+/**
+ * Represents a find command.
+ */
 public class FindCommand extends Command {
+    /** Keyword to find in task strings. */
     private String wordToFind;
 
+    /**
+     * Constructs a find command.
+     * @param keyword the keyword/phrase to find in task strings.
+     */
     public FindCommand(String keyword) {
         this.wordToFind = keyword;
         super.isExit = false;
     }
 
+    /**
+     * Finds tasks that contain keyword.
+     * @param taskList TaskList agent to handle list of tasks.
+     * @param ui Ui in charge of printing.
+     * @param storage Storage agent in charge of reading/writing to file.
+     * @return String containing list of tasks that have that keyword.
+     */
     @Override
     public String execute(TaskList taskList, Ui ui, Storage storage) {
         List<Task> foundTasks = taskList.findTasks(this.wordToFind);
-
         if (foundTasks.isEmpty()) {
             return ui.dukeRespond("Sorry! I can't find any matching tasks in your list. Try another phrase?");
-        } else {
-            return ui.dukeRespond(getStringArray(foundTasks));
         }
+
+        return ui.dukeRespond(getStringArray(foundTasks));
     }
 
     private String[] getStringArray(List<Task> tasks) {
